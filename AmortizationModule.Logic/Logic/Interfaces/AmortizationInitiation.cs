660d3f1fc719ac2e4d960bd7f86fef50377a2700
Eirik Håland Berglund <eirik@escali.no>
2017-07-11 10:43:03 +0200

    Added vs2015 bugfix

--- AmortizationModule.Logic/Logic/Interfaces/AmortizationInitiation.cs
+++ AmortizationModule.Logic/Logic/Interfaces/AmortizationInitiation.cs
@@ -2,18 +2,36 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AmortizationModule.Logic
 {
     public abstract class AmortizationInitiation
     {
         private AmortizationTransaction transaction;
-        public DateTime TransactionDate { get => transaction.TransactionDate; set => transaction.TransactionDate = value; }
+        public DateTime TransactionDate
+        {
+            get
+            {
+                return transaction.TransactionDate;
+            }
+            set
+            {
+                transaction.TransactionDate = value;
+            }
+        }
 
         private List<AmortizationLink> links;
-        public List<AmortizationLink> Links { get => links; set => links = value; }
+        public List<AmortizationLink> Links
+        {
+            get
+            {
+                return links;
+            }
+            set
+            {
+                links = value;
+            }
+        }
         public AmortizationInitiation(AmortizationTransaction transaction)
         {
             this.transaction = transaction;
