cd8603df33c80c0ef0a4e177449cb1ab77419832
Eirik Håland Berglund <eirik@escali.no>
2017-07-13 09:46:50 +0200

    Refined tests

--- AmortizationModule.Logic.Tests/UnitTestM2.cs
+++ AmortizationModule.Logic.Tests/UnitTestM2.cs
@@ -47,7 +47,7 @@ namespace AmortizationModule.Logic
             Input.AmortizationSecurity.InterestTerms = new Dictionary<DateTime, double> {
                 { new DateTime(2015,01,01), 0.047},
                 { new DateTime(2016,01,01), 0.047},
-                { new DateTime(2017,01,01), 0.047},
+                { new DateTime(2017,01,01), 0.0471287671232877},
                 { new DateTime(2018,01,01), 0.047},
                 { new DateTime(2019,01,01), 0.047},
                 { new DateTime(2020,01,01), 0.047},
@@ -62,11 +62,11 @@ namespace AmortizationModule.Logic
             BuildHelper.Transaction("01.01.2014",4,1,"V-01",1250000,0.96,1,"NOK",1),
             BuildHelper.Transaction("01.01.2015",9,1,"V-02",58750,1,2,"NOK",1),
             BuildHelper.Transaction("01.01.2016",9,1,"V-03",58750,1,3,"NOK",1),
-            BuildHelper.Transaction("01.01.2017",9,1,"V-04",58750,1,4,"NOK",1),
+            BuildHelper.Transaction("01.01.2017",9,1,"V-04",58910.9589,1,4,"NOK",1),
             BuildHelper.Transaction("01.01.2018",9,1,"V-05",58750,1,5,"NOK",1),
             BuildHelper.Transaction("01.01.2019",9,1,"V-06",58750,1,6,"NOK",1),
             BuildHelper.Transaction("01.01.2020",9,1,"V-07",58750,1,7,"NOK",1),
-            BuildHelper.Transaction("02.01.2020",68,1,"V-08",1250000,1,8,"NOK",1)};
+            BuildHelper.Transaction("01.01.2020",68,1,"V-08",1250000,1,8,"NOK",1)};
 
 
             return Input;
@@ -78,7 +78,7 @@ namespace AmortizationModule.Logic
         {
             AmortizationInput input = SetUpSimpleBond3();
             AmortizationOutput output = CommandHelper.GenerateAmortizationOutput(input);
-            AssertHelper.VerifyOutputTotalAccumulatedAmortizationEquals("01.01.2019", 8992.647, output);
+            AssertHelper.VerifyOutputTotalAccumulatedAmortizationEquals("01.01.2019", 40517.14696, output);
         }
 
     }
--- AmortizationModule.Logic.Tests/UnitTestMSerial.cs
+++ AmortizationModule.Logic.Tests/UnitTestMSerial.cs
@@ -37,7 +37,7 @@ namespace AmortizationModule.Logic
             Input.Settings = BuildHelper.CreateSettings(
             Method: 1,
             OutputAggregated: true,
-            InterestMethod: 1);
+            InterestMethod: 2);
 
             Input.AmortizationSecurity.Instalments = new Dictionary<DateTime, double>() {
                 { new DateTime(2005,12,31), 0.2},
@@ -49,15 +49,15 @@ namespace AmortizationModule.Logic
 
             Input.AmortizationSecurity.InterestTerms = new Dictionary<DateTime, double> {
                 { new DateTime(2005,06,30), 0.06},
-                { new DateTime(2005,12,31), 0.06},
-                { new DateTime(2006,06,30), 0.048},
-                { new DateTime(2006,12,31), 0.048},
-                { new DateTime(2007,06,30), 0.036},
-                { new DateTime(2007,12,31), 0.036},
-                { new DateTime(2008,06,30), 0.024},
-                { new DateTime(2008,12,31), 0.024},
-                { new DateTime(2009,06,30), 0.012},
-                { new DateTime(2009,12,31), 0.012},
+                { new DateTime(2005,12,31), 0.06133333333333},
+                { new DateTime(2006,06,30), 0.04826666666667},
+                { new DateTime(2006,12,31), 0.04906666666667},
+                { new DateTime(2007,06,30), 0.0362},
+                { new DateTime(2007,12,31), 0.0368},
+                { new DateTime(2008,06,30), 0.02426666666667},
+                { new DateTime(2008,12,31), 0.02453333333333},
+                { new DateTime(2009,06,30), 0.01206666666667},
+                { new DateTime(2009,12,31), 0.01226666666667},
             };
 
             Input.InterestRates = BuildHelper.CreateInterestRates(new Dictionary<string, double>(){
@@ -66,19 +66,19 @@ namespace AmortizationModule.Logic
             Input.AmortizationTransactions = new List<AmortizationTransaction>(){
                 BuildHelper.Transaction("01.01.2005",4,1,"V-01",100000,0.95409,1,"NOK",1),
                 BuildHelper.Transaction("30.06.2005",9,1,"V-02",6000,1,2,"NOK",1),
-                BuildHelper.Transaction("31.12.2005",9,1,"V-03",6000,1,3,"NOK",1),
+                BuildHelper.Transaction("31.12.2005",9,1,"V-03",6133.333333,1,3,"NOK",1),
                 BuildHelper.Transaction("31.12.2005",68,1,"V-04",20000,1,4,"NOK",1),
-                BuildHelper.Transaction("30.06.2006",9,1,"V-05",4800,1,5,"NOK",1),
-                BuildHelper.Transaction("31.12.2006",9,1,"V-06",4800,1,6,"NOK",1),
+                BuildHelper.Transaction("30.06.2006",9,1,"V-05",4826.666667,1,5,"NOK",1),
+                BuildHelper.Transaction("31.12.2006",9,1,"V-06",4906.666667,1,6,"NOK",1),
                 BuildHelper.Transaction("31.12.2006",68,1,"V-07",20000,1,7,"NOK",1),
-                BuildHelper.Transaction("30.06.2007",9,1,"V-08",3600,1,8,"NOK",1),
-                BuildHelper.Transaction("31.12.2007",9,1,"V-09",3600,1,9,"NOK",1),
+                BuildHelper.Transaction("30.06.2007",9,1,"V-08",3620,1,8,"NOK",1),
+                BuildHelper.Transaction("31.12.2007",9,1,"V-09",3680,1,9,"NOK",1),
                 BuildHelper.Transaction("31.12.2007",68,1,"V-10",20000,1,10,"NOK",1),
-                BuildHelper.Transaction("30.06.2008",9,1,"V-11",2400,1,11,"NOK",1),
-                BuildHelper.Transaction("31.12.2008",9,1,"V-12",2400,1,12,"NOK",1),
+                BuildHelper.Transaction("30.06.2008",9,1,"V-11",2426.666667,1,11,"NOK",1),
+                BuildHelper.Transaction("31.12.2008",9,1,"V-12",2453.333333,1,12,"NOK",1),
                 BuildHelper.Transaction("31.12.2008",68,1,"V-13",20000,1,13,"NOK",1),
-                BuildHelper.Transaction("30.06.2009",9,1,"V-14",1200,1,14,"NOK",1),
-                BuildHelper.Transaction("31.12.2009",9,1,"V-15",1200,1,15,"NOK",1),
+                BuildHelper.Transaction("30.06.2009",9,1,"V-14",1206.666667,1,14,"NOK",1),
+                BuildHelper.Transaction("31.12.2009",9,1,"V-15",1226.666667,1,15,"NOK",1),
                 BuildHelper.Transaction("31.12.2009",68,1,"V-16",20000,1,16,"NOK",1)
             };
 
@@ -92,7 +92,7 @@ namespace AmortizationModule.Logic
         {
             AmortizationInput input = SetUpBondsSerial();
             AmortizationOutput output = CommandHelper.GenerateAmortizationOutput(input);
-            AssertHelper.VerifyOutputTotalAccumulatedAmortizationEquals("30.06.2008", 327.243, output);
+            AssertHelper.VerifyOutputTotalAccumulatedAmortizationEquals("30.06.2008", 3874, output);
         }
 
     }
